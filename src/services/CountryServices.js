09f85d39b92b6d93dfc1c8c320a23db4f2d527ef
valeviit <valeviit@icloud.com>
2022-12-04 23:52:15 +0100

    create services file

---
+++ src/services/CountryServices.js
@@ -0,0 +1,16 @@
+import Country from "../models/Country";
+
+class CountryServices {
+    //create new country for the list
+    async create(country){
+        const createCountry = await Country.create(country)
+        return createCountry
+    }
+
+    async getAll(){
+        const countries= await Country.find();
+        return countries
+    }
+}
+
+export default new CountryServices;
